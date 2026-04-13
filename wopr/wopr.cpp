e4fc2808c74bda8b0f40a23de5b5baa165704622
Jason Hall <jasonbrianhall@gmail.com>
2026-04-13 16:00:59 -0400

    Laetst

--- wopr/wopr.cpp
+++ wopr/wopr.cpp
@@ -34,10 +34,10 @@ static const GameEntry GAMES[] = {
     { "TIC-TAC-TOE",              "TIC",         WoprGame::TIC_TAC_TOE},
     { "MINESWEEPER",              "MINESWEEPER", WoprGame::MINESWEEPER},
     { "ZORK",                     "ZORK",        WoprGame::ZORK       },
-    { "BASIC",                    "BASIC",       WoprGame::BASIC      },
+    /*{ "BASIC",                    "BASIC",       WoprGame::BASIC      }, */
     { "WIZARD'S CASTLE",          "WIZARD",      WoprGame::WIZARD     },
 };
-static const int GAME_COUNT = 8;
+static const int GAME_COUNT = 7;
 
 static const char *VALID_USER = "FALKEN";
 static const char *VALID_PASS = "JOSHUA";
@@ -201,14 +201,14 @@ static void launch_game(WoprState *w, WoprGame game) {
             set_phase(w, WoprPhase::PLAYING_ZORK);
             wopr_zork_enter(w);
             break;
-        case WoprGame::BASIC:
+        /*case WoprGame::BASIC:
             push_line(w, "INITIATING: BASIC");
             push_line(w, "");
             push_line(w, "  LOADING PROGRAMMING LANGUAGE...");
             push_line(w, "");
             set_phase(w, WoprPhase::PLAYING_BASIC);
             wopr_basic_enter(w);
-            break;
+            break;*/
         case WoprGame::WIZARD:
             push_line(w, "INITIATING: WIZARD'S CASTLE");
             push_line(w, "");
